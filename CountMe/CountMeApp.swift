9bd0f5b157f288658ec4a1c4c1cfbba512e5f839
Imo Madjid <mhmmadjid@gmail.com>
2025-03-19 12:50:32 +0700

    Initial Commit

---
+++ CountMe/CountMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountMeApp.swift
+//  CountMe
+//
+//  Created by Imo Madjid on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CountMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountMeTests/CountMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountMeTests.swift
+//  CountMeTests
+//
+//  Created by Imo Madjid on 3/19/25.
+//
+
+import Testing
+@testable import CountMe
+
+struct CountMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
